e819e0ef0c957eefed03671c1c46424a05d3ab35
Ismail <Ismailchaudry123@icloud.com>
2026-04-19 17:50:50 -0500

    added divison function

--- welcome.py
+++ welcome.py
@@ -1,5 +1,5 @@
 number_1 = input("First number: ")
-operation_1 = input("What operation do you want to do? Enter +, -, %, *: ")
+operation_1 = input("What operation do you want to do? Enter +, -, /, *: ")
 number_2 = input("Second number: ")
 
 number_1 = int(number_1)
@@ -11,6 +11,8 @@ elif operation_1 == '-':
     print(number_1 - number_2)
 elif operation_1 == '*':
     print(number_1 * number_2)
+elif operation_1 == '/':
+    print(number_1 / number_2)
 elif operation_1 == '%':
     print(number_1 % number_2)
 else:
