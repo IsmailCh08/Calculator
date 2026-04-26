acf3baa22572edbce825ab5240ab60a2fac34f60
Ismail <Ismailchaudry123@icloud.com>
2026-04-26 10:38:04 -0500

    new up and running calculator

--- welcome.py
+++ welcome.py
@@ -1,17 +1,17 @@
 number = 0
 operation = 0
-
+number_2 = 0
 def add(x,y):
-    x + y
+    return x + y
 
 def multiply(x,y):
-    x * y
+    return x * y
 
 def divide(x,y):
-    x/y
+    return x/y
 
 def subtract(x,y):
-    x-y
+    return x-y
 
 operation_lookup = {
     "+": add,
@@ -20,10 +20,21 @@ operation_lookup = {
     "*": multiply
 }
 
-while number != "quit":
+while True:
     number = input("Enter a number (or 'quit' to exit): ")
-    number = int(number)
+    if number == "quit":
+        break
+    number = float(number)
     operation = input("Enter an operation (+, -, *, /): ")
-    if operation != ["+", "-", "/", "*"]:
+    if operation not in ["+", "-", "/", "*"]:
         print("Invalid operation. Please try again.")
+    number_2 = input("Enter second number (or 'quit' to exit): ")
+    number_2 = float(number_2)
+    break
+
+if operation in operation_lookup:
+    used_Operation = operation_lookup[operation]
+    result = used_Operation(number, number_2)
+    print(result)
+
         
